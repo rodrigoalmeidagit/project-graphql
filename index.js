b2866637b6b7ba0c9dde135836f6a96ed319ea2e
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-08-21 23:49:43 -0300

    stylerefactor: change newUser arguments to UserInput input schema

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { importSchema } = require('graphql-import')
-const resolvers = require('./resolvers')
+const { importSchema } = require('graphql-import');
+const resolvers = require('./resolvers');
 
-const schemaPath = './schema/index.graphql'
+const schemaPath = './schema/index.graphql';
 
 const server = new ApolloServer({
   typeDefs: importSchema(schemaPath),
   resolvers
-})
+});
 
 server.listen().then(({ url }) => {
   console.log(`Running in ${url}`);
-})
+});
