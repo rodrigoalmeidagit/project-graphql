5f961ef12618dc63e57accc8473674677f5df157
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-08-25 00:30:56 -0300

    refactor(getuser/args): change getUser args to UserFilter input

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,14 +1,24 @@
 const { usersList, profiles } = require('../data/db')
 
+const { userIndex } = require('../functions/user-index')
+
 module.exports = {
   users() {
     return usersList
   },
 
-  getUser(_, { id }) {
-    const selected = usersList
-      .filter(u => u.id === id)
-    return selected ? selected[0] : null
+  getUser(_, { filter }) {
+    const selectedUser = userIndex(filter)
+
+    if (selectedUser < 0) null
+
+    return usersList[selectedUser]
+
+    // const selected = usersList
+    //   .filter(u => u.id === id)
+    // return selected ? selected[0] : null
+
+    //selected[0] pq o filter retorna um array
   },
 
   profiles() {
@@ -18,6 +28,9 @@ module.exports = {
   profile(_, { id }) {
     const selectedProfile = profiles
       .filter(p => p.id === id)
-    return selectedProfile ? selectedProfile[0] : null
+
+    return selectedProfile
+      ? selectedProfile[0]
+      : null
   }
 }
