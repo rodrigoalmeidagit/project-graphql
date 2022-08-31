2af01060cb4b46d8755babc89c63b605a0fb29f4
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2022-08-30 23:22:56 -0300

    feat(profile/files): add graphql and js profile files

--- resolvers/Profile/Profile.js
+++
@@ -1,8 +0,0 @@
-const { profiles } = require('../../data/db')
-
-module.exports = {
-  profile(user) {
-    const selectedProfile = profiles.filter(p => p.id === user.profile)
-    return selectedProfile ? selectedProfile[0] : null
-  }
-}
